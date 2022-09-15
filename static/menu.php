885e14d1642dce00e86dde39bb2e0dd9a9ec34fa
ubuntuVM_DEV <ubuntu@anir0y.vm>
2022-09-15 18:29:47 +0530

    vwa source code
    Signed-off-by: ubuntuVM_DEV <ubuntu@anir0y.vm>

---
+++ static/menu.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="css/menu.css" type="text/css">
+<nav>
+<ul class="wrapper">
+	<li><a href="home.php">Home</a></li>
+	<li><a href="admin.php">admin_panel</a></li>
+	<li><a href="upload.php">Upload</a></li>
+  <li><a href="search.php">search users</a></li>
+	<li><a href="greetings.php">Echo</a></li>
+	<li><a href="ping.php">Is Online?</a></li>
+  <li><a href="logout.php">Log Out</a></li>
+
+</ul>
+</nav>
